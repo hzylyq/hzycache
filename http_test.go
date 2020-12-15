1541efea7e73533ad29d32b2bb3679bc8b1cbd76
胡志扬 <164177565@qq.com>
2020-12-15 18:32:13 +0800

    feat: http

---
+++ http_test.go
@@ -0,0 +1,30 @@
+package hzycache
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"testing"
+)
+
+func TestHttpPool_ServerHttp(t *testing.T) {
+	var db = map[string]string{
+		"Tom":  "630",
+		"Jack": "589",
+		"Sam":  "567",
+	}
+
+	NewGroup("scores", 2<<10, GetterFunc(
+		func(key string) ([]byte, error) {
+			log.Println("[SlowDB] search key", key)
+			if v, ok := db[key]; ok {
+				return []byte(v), nil
+			}
+			return nil, fmt.Errorf("%s not exist", key)
+		}))
+
+	addr := "localhost:9999"
+	peers := NewHttpPool(addr)
+	log.Println("hzycache is running at", addr)
+	log.Fatal(http.ListenAndServe(addr, peers))
+}
